9237aaad01cc14a3bf1bdf3ed7e2e3a5e6debe73
Miikakyl <kylmanen.miika@gmail.com>
2024-06-08 20:16:03 +0300

    Testing file structure changed

--- src/__tests__/unit/DataInputs.test.js
+++ src/__tests__/unit/DataInputs.test.js
@@ -49,4 +49,14 @@ describe(DataInputs, () => {
         expect(previewInput.value).toBe("100")
 
     })
+    
+    it("should upload file and then display the file name", async () => {
+        render(<DataInputs />)
+        const testFile = new File(['1,2,3,4,5'], 'foo.csv', {type: 'text/plain'})
+        const fileInput = screen.getByTestId("file-input")
+    
+        fireEvent.change(fileInput, { target: { files: [testFile]}})
+    
+        expect(screen.getByTestId("file-name").textContent).toBe("foo.csv")
+    })
 })
